9f987d02f68ad42655caf89df362a0dc17d8ceef
Muhammed Salih Km <salihkm000@gmail.com>
2025-04-07 14:43:09 +0530

    origin added

--- index.js
+++ index.js
@@ -14,10 +14,23 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 // Use CORS with specific origin
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://iot-ads-frontend.vercel.app'],
-  credentials: true, // only if you're sending cookies
-}));
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = ['http://localhost:5173', 'https://iot-ads-frontend.vercel.app'];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 
 const port = process.env.PORT || 3000;
